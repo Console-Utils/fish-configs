a5f9e08448d28f7142a07d59b690e89de6865a9e
Emily Seville <emilyseville7cf@gmail.com>
2021-10-28 22:37:25 +1000

    prompt divide to several functions

--- config.fish
+++ config.fish
@@ -4,15 +4,22 @@ not status is-interactive && exit
 
 fish_add_path "/home/emilyseville7cfg/.local/bin"
 
-function print_pwd
-  set -l directory $PWD
-  set -l directory (string replace --regex '^'"$HOME" '~' $directory)
+set -g MINE_PATH "~/Documents/mine"
+set -g WORK_PATH "~/Documents/work"
 
-  set -l MINE_PATH "~/Documents/mine"
-  set -l WORK_PATH "~/Documents/work"
+function transform_path --description 'Replaces $HOME with tilde and mine/work dirs with special keywords'
+  set -l directory $argv[1]
+
+  set -l directory (string replace --regex '^'"$HOME" '~' $directory)
 
   set -l directory (string replace --regex '^'"$MINE_PATH" '\$MINE_PATH' $directory)
   set -l directory (string replace --regex '^'"$WORK_PATH" '\$WORK_PATH' $directory)
+
+  echo $directory
+end
+
+function colorize_path --description 'Colorizes path'
+  set -l directory $argv[1]
 
   set -l RESET_COLOR (set_color normal)
 
@@ -22,8 +29,8 @@ function print_pwd
   set -l MINE_COLOR (set_color --bold)
   set -l WORK_COLOR (set_color --bold)
 
-  set -l directory (string replace --regex '^(\$MINE_PATH)' "$MINE_COLOR"'$1'"$RESET_COLOR" $directory)
-  set -l directory (string replace --regex '^(\$WORK_PATH)' "$WORK_COLOR"'$1'"$RESET_COLOR" $directory)
+  set -l directory (string replace --regex '^(\$MINE_PATH)' "$MINE_COLOR"'🏠$1'"$RESET_COLOR" $directory)
+  set -l directory (string replace --regex '^(\$WORK_PATH)' "$WORK_COLOR"'🏢$1'"$RESET_COLOR" $directory)
 
   set -l directory $PATH_COLOR$directory
   set -l directory (string replace --regex --all '/' "$SLASH_COLOR"'/'"$PATH_COLOR" $directory)
@@ -32,9 +39,18 @@ function print_pwd
   echo $directory
 end
 
+function cwd_prompt
+  set -l directory $PWD
+
+  set -l directory (transform_path $directory)
+  set -l directory (colorize_path $directory)
+
+  echo $directory
+end
+
 function fish_prompt
   set -l user_char '💲'
   fish_is_root_user && set user_char '⭐💲'
 
-  echo -s (set_color yellow) (print_pwd) $user_char
+  echo -s (set_color yellow) (cwd_prompt) $user_char
 end
