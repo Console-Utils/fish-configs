9e5a3d58c0264239e6acd3fd75e2009c526d49c4
Emily Seville <emilyseville7cf@gmail.com>
2021-11-01 19:49:47 +1000

    Move fish_prompt to functions/fish_prompt.fish

--- config.fish
+++ config.fish
@@ -34,13 +34,4 @@ function create_abbrevations --description 'Creates missing abbrevations'
   end
 end
 
-function fish_prompt
-  set --local statuses $pipestatus
-
-  set --local user_char '💲'
-  fish_is_root_user && set user_char '⭐💲'
-
-  echo -s (set_color yellow) (__cwd_prompt) (__pipestatus_prompt $statuses) $user_char
-end
-
 create_abbrevations
