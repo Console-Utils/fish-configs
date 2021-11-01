c140c0fca34ee93070628b8fb7ad9d129fa8c7ee
Emily Seville <emilyseville7cf@gmail.com>
2021-11-01 19:19:38 +1000

    Create __pipestatus_prompt.fish

---
+++ functions/__pipestatus_prompt.fish
@@ -0,0 +1,16 @@
+function __pipestatus_prompt --description 'Prints $pipestatus in human-readable format'
+  set --local statuses $argv
+
+  set --query PROMPT_PIPESTATUS_BRACKET_COLOR || set --universal PROMPT_PIPESTATUS_BRACKET_COLOR (set_color red)
+  set --query PROMPT_PIPESTATUS_STATUS_COLOR || set --universal PROMPT_PIPESTATUS_STATUS_COLOR (set_color brred)
+  set --query PROMPT_PIPESTATUS_DELIMITER_COLOR || set --universal PROMPT_PIPESTATUS_DELIMITER_COLOR (set_color purple)
+
+  echo -n $PROMPT_PIPESTATUS_BRACKET_COLOR'['
+
+  for i in (seq 1 (count $statuses))
+    echo -n $PROMPT_PIPESTATUS_STATUS_COLOR$statuses[$i]
+    test $i -lt (count $statuses) && echo -n $PROMPT_PIPESTATUS_DELIMITER_COLOR'|'
+  end
+
+  echo -n $PROMPT_PIPESTATUS_BRACKET_COLOR']'
+end
