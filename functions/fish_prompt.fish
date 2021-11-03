bde4127e329a3f1415734d616501fe3cb72cb4a9
Emily Seville <emilyseville7cf@gmail.com>
2021-11-03 22:02:01 +1000

    Error shown when not all envs defined

--- functions/fish_prompt.fish
+++ functions/fish_prompt.fish
@@ -1,8 +1,19 @@
 function fish_prompt
-  set --local statuses $pipestatus
+  set --query PROMPT_ERROR_SIGN || set --local PROMPT_ERROR_SIGN (set_color brred)'✘'(set_color normal)
+
+  set --query PROMPT_PATH_COLOR || set --local PROMPT_PATH_COLOR (set_color brred)
+
+  set --local STATUSES $pipestatus
 
   set --local user_char '💲'
   fish_is_root_user && set user_char '⭐💲'
 
-  echo -s (set_color yellow) (__cwd_prompt) (__pipestatus_prompt $statuses) $user_char
+  if ! set --query MINE_PATH WORK_PATH
+    echo -sn $PROMPT_ERROR_SIGN'Can\'t show correct prompt because any of the following variables are undefined: '\
+      $PROMPT_PATH_COLOR\"MINE_PATH\"$RESET_COLOR','\
+      $PROMPT_PATH_COLOR\"WORK_PATH\"$RESET_COLOR $user_char
+    return
+  end
+
+  echo -s (set_color yellow) (__cwd_prompt) (__pipestatus_prompt $STATUSES) $user_char
 end
