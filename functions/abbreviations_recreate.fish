36936026aafb4bc09c9c48c991c66c916ab9da1f
Emily Grace Seville <emilyseville7cf@gmail.com>
2021-11-10 20:24:45 +1000

    abbreviations_recreate clean-up:
    - $HOME use where necessary
    - --argument-names use

--- functions/abbreviations_recreate.fish
+++ functions/abbreviations_recreate.fish
@@ -1,8 +1,5 @@
-function __create_abbreviation --description 'Creates specified abbreviation'
+function __create_abbreviation --argument-names ABBREVIATION COMMAND --description 'Creates specified abbreviation'
   set --local NOT_VALID_ABBREVIATION_ERROR 1
-  
-  set --local ABBREVIATION $argv[1]
-  set --local COMMAND $argv[2]
 
   set --query PROMPT_SUCCESS_SIGN || set --local PROMPT_SUCCESS_SIGN (set_color brgreen)'✔'(set_color normal)
   set --query PROMPT_ERROR_SIGN || set --local PROMPT_ERROR_SIGN (set_color brred)'✘'(set_color normal)
@@ -23,8 +20,8 @@ function __create_abbreviation --description 'Creates specified abbreviation'
 end
 
 function abbreviations_recreate --description 'Creates missing abbreviations'
-  set abbreviations cmi 'cd /home/emilyseville7cfg/Documents/mine/'
-  set --append abbreviations cwo 'cd /home/emilyseville7cfg/Documents/work/'
+  set abbreviations cmi "cd $HOME/Documents/mine/"
+  set --append abbreviations cwo "cd $HOME/Documents/work/"
   set --append abbreviations e echo
   set --append abbreviations f for
   set --append abbreviations gcl 'git clone'
@@ -35,7 +32,7 @@ function abbreviations_recreate --description 'Creates missing abbreviations'
   set --append abbreviations gun 'rm -rf .git'
   set --append abbreviations i 'if test'
   set --append abbreviations pf printf
-  set --append abbreviations r 'source /home/emilyseville7cfg/.config/fish/config.fish'
+  set --append abbreviations r "source $HOME/.config/fish/config.fish"
   set --append abbreviations w 'while test'
 
   set --local errors 0
